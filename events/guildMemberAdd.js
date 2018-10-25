99ef82a7ec436ee82fce671f1b18b1a36dee6b43
sucukluekmekpeynir <44277051+sucukluekmekpeynir@users.noreply.github.com>
2018-10-25 21:50:53 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => {
     let username = member.user.username;
-    member.send('Hoş geldin **' + username + '**!');
+    member.sendMessage('Hoş geldin ' + username + '!');
     member.guild.defaultChannel.send('hg '+username+'');
 };
