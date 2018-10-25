4dcc2499ba85b96ca4751a6a6d0111e6ea72717d
sucukluekmekpeynir <44277051+sucukluekmekpeynir@users.noreply.github.com>
2018-10-25 23:17:30 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,8 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.sendMessage('Hoş geldin ' + username + '!');
-    member.guild.defaultChannel.send('hg '+username+'');
-};
+client.on('guildMemberAdd', uye =&gt; {
+  // Belirli bir kanala mesaj atması için kod. Buna göre "#gelen-giden" kanalına mesaj atacak:
+  const channel = uye.guild.channels.find('name', 'giriş-çıkış');
+  // Eğer kanal yoksa, hiçbir şey yapma:
+  if (!channel) return;
+  // Üyeyi etiketleyerek "#gelen-giden" kanalına mesaj at:
+  channel.send(`Sunucuya hoş geldin, ${uye}`);
+});
